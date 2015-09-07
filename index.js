76bd4debbb9cc5e8cff960bf0368c40387220c55
Eric Collins <emc@cavs.msstate.edu>
2015-09-07 14:55:03 -0500

    Add management and updates for multiple objects (static and dynamic). Add communication of initial states of dynamic objects.

--- index.js
+++ index.js
@@ -1,50 +1,18 @@
-// INITIALIZE SOCKET INTERFACE
-var sockjs_url = '/deckard';
-var sockjs = new SockJS(sockjs_url);
-var socketOpen = false;
-var socketReady = false;
-
-sockjs.onopen = function() {
-    socketOpen = true;
-    socketReady = true;
-    console.log('socket opened: ' + sockjs.protocol);
-};
-
-sockjs.onmessage = function(e) {
-    socketReady = true;
-    updateGraphics(JSON.parse(e.data));
-};
-
-sockjs.onclose = function() {
-    socketOpen = false;
-    socketReady = false;
-    console.log('socket closed');
-};
-
-// OBJECT STATE
-var State = function() {
-};
-
-// GRAPHICS INTERFACE
-var render_stats, physics_stats;
-var camera, scene, renderer;
-var clock = new THREE.Clock();
-
-var WIDTH = window.innerWidth;
-var HEIGHT = window.innerHeight;
-var backgroundColor = 0x444444;
-var group;
-
+// ENTRY FUNCTION - initialize everything
 function init() {
     initGUI();
     initScene();
+    initSocket();
     animate();
-};
+}
 
 //--------------------------------------------------------------------
+// PAGE INTERFACE ELEMENTS
+var container, render_stats, physics_stats, renderer;
+
 // Initialize the visible elements displayed on the web page
 function initGUI() {
-    var container = document.createElement('div');
+    container = document.createElement('div');
     document.body.appendChild(container);
 
     // Graph that displays the rate at which the scene is rendering.
@@ -64,20 +32,49 @@ function initGUI() {
     // The main viewport where the scene will be rendered.
 		renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setClearColor( backgroundColor, 1 );
-    renderer.setSize(WIDTH, HEIGHT);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 		renderer.shadowMapEnabled = true;
 		renderer.shadowMapSoft = true;
     container.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', onWindowResize, false);
 }
 
+//--------------------------------------------------------------------
+// GRAPHICAL SCENE ELEMENTS
+var camera, scene;
+var backgroundColor = 0x444444;
+var staticObjects = [];
+var dynamicObjects = [];
+
+function DynamicObject(threeObj) {
+    // Associated THREE.js object
+    this.obj = threeObj;
+    // Linear velocity
+    this.dpxdt = 0;
+    this.dpydt = 0;
+    this.dpzdt = 0;
+    // Angular velocity
+    this.drxdt = 0;
+    this.drydt = 0;
+    this.drzdt = 0;
+};
+
+DynamicObject.prototype.update = function(dt) {
+    this.obj.rotation.x += this.drxdt*dt;
+    this.obj.rotation.y += this.drydt*dt;
+};
+
+// Initialize the scene graph
 function initScene() {
     var aspect = window.innerWidth / window.innerHeight;
+
     camera = new THREE.PerspectiveCamera( 75, aspect, 1, 1000 );
 	  camera.position.z = 500;
 
 	  scene = new THREE.Scene();
 
-    var geom = new THREE.IcosahedronGeometry( 200, 1 );
+    var geom = new THREE.IcosahedronGeometry( 100, 1 );
 	  var mat1 = new THREE.MeshBasicMaterial( { color: 0xFFFFFF } );
 	  var mat2 = new THREE.MeshBasicMaterial( {
         color: backgroundColor,
@@ -86,42 +83,131 @@ function initScene() {
     } );
 	  var mesh1 = new THREE.Mesh( geom, mat1 );
 	  var mesh2 = new THREE.Mesh( geom, mat2 );
-    group = new THREE.Group();
-    group.add( mesh1 );
-    group.add( mesh2 );
-	  scene.add( group );
+    var obj = new THREE.Object3D();
+    obj.add( mesh1 );
+    obj.add( mesh2 );
+
+    for (var i=0; i<2; ++i) {
+        dynamicObjects[i] = new DynamicObject(obj.clone());
+        dynamicObjects[i].obj.translateX(-200 + i*400);
+	      scene.add( dynamicObjects[i].obj );
+    }
 }
 
-var dpxdt = 0;
-var dpydt = 0;
-var drxdt = 0;
-var drydt = 0;
+//--------------------------------------------------------------------
+// SOCKET INTERFACE
+var sockjs;
+var socketOpen = false;
+var socketReady = false;
+function initSocket() {
+    sockjs = new SockJS('/deckard');
 
+    sockjs.onopen = function() {
+        socketOpen = true;
+        socketReady = true;
+        console.log('socket opened: ' + sockjs.protocol);
+        registerObjects();
+    };
+
+    sockjs.onmessage = function(e) {
+        socketReady = true;
+        var data = JSON.parse(e.data);
+        if (data.type = 'update') {
+            var dt = data.delta;
+            for (var i=0; i<data.objs.length; ++i) {
+                var id = data.objs[i].id;
+                dynamicObjects[id].dpxdt = data.objs[i].dx;
+                dynamicObjects[id].dpydt = data.objs[i].dy;
+                dynamicObjects[id].drxdt = data.objs[i].rx;
+                dynamicObjects[id].drydt = data.objs[i].ry;
+            }
+            physics_stats.update();
+        }
+    };
+
+    sockjs.onclose = function() {
+        socketOpen = false;
+        socketReady = false;
+        console.log('socket closed');
+    };
+}
+
+//--------------------------------------------------------------------
+// REGISTER OBJECTS WITH SIMULATOR
+function registerObjects() {
+    if (socketOpen && socketReady) {
+        console.log("Registering " + dynamicObjects.length +
+                    " dynamic objects.");
+        for (var i=0; i<dynamicObjects.length; ++i) {
+            var jsonObj = {
+                type: 'dynamic',
+                id: i,
+                pos: {
+                    x: dynamicObjects[i].obj.position.x,
+                    y: dynamicObjects[i].obj.position.y,
+                    z: dynamicObjects[i].obj.position.z
+                },
+                rot: {
+                    x: dynamicObjects[i].obj.rotation.x,
+                    y: dynamicObjects[i].obj.rotation.y,
+                    z: dynamicObjects[i].obj.rotation.z
+                }
+            };
+            sockjs.send(JSON.stringify(jsonObj));
+        }
+        console.log("Registering " + staticObjects.length +
+                    " static objects.");
+        for (var i=0; i<staticObjects.length; ++i) {
+            var jsonObj = {
+                type: 'static',
+                id: i,
+                pos: {
+                    x: staticObjects[i].obj.position.x,
+                    y: staticObjects[i].obj.position.y,
+                    z: staticObjects[i].obj.position.z
+                },
+                rot: {
+                    x: staticObjects[i].obj.rotation.x,
+                    y: staticObjects[i].obj.rotation.y,
+                    z: staticObjects[i].obj.rotation.z
+                }
+            };
+            sockjs.send(JSON.stringify(jsonObj));
+        }
+    }
+}
+
+//--------------------------------------------------------------------
+// SCENE UPDATES AND ANIMATIONS
+var clock = new THREE.Clock();
+var startTime = Date.now();
 function animate() {
     requestAnimationFrame( animate );
     var delta = clock.getDelta();
     // Wait for previous update to complete
     if (socketOpen && socketReady) {
         var jsonObj = {
-            dt: delta
+            type: 'update',
+            time: Date.now() - startTime,
+            delta: delta
         };
         sockjs.send(JSON.stringify(jsonObj));
         socketReady = false;
     }
-    
-    group.rotation.x += drxdt*delta;
-    group.rotation.y += drydt*delta;
+
+    for (var i=0; i<dynamicObjects.length; ++i) {
+        dynamicObjects[i].update(delta);
+    }
 
     render();
     render_stats.update();
 }
 
-function updateGraphics(data) {
-    dpxdt = data.pos.dx/data.dt;
-    dpydt = data.pos.dy/data.dt;
-    drxdt = data.rot.dx/data.dt;
-    drydt = data.rot.dy/data.dt;
-    physics_stats.update();
+function onWindowResize(event) {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    render();
 }
 
 function render() {
--- server.js
+++ server.js
@@ -2,29 +2,71 @@ var http = require('http');
 var sockjs = require('sockjs');
 var node_static = require('node-static');
 
+// 1. SockJS communication server
 var sockjs_opts = {
     sockjs_url: "http://cdn.jsdelivr.net/sockjs/0.3.4/sockjs.min.js"
 };
 
 var sockjs_server = sockjs.createServer(sockjs_opts);
 
+var dynamicObjects = [];
+var staticObjects = [];
+
+function DynamicObject(json) {
+    this.id = json.id;
+    this.x = json.pos.x;
+    this.y = json.pos.y;
+    this.z = json.pos.z;
+    this.rx = json.rot.x;
+    this.ry = json.rot.y;
+    this.rz = json.rot.z;
+}
+
+function StaticObject(json) {
+    this.id = json.id;
+    this.x = json.pos.x;
+    this.y = json.pos.y;
+    this.z = json.pos.z;
+    this.rx = json.rot.x;
+    this.ry = json.rot.y;
+    this.rz = json.rot.z;
+}
+
 sockjs_server.on( 'connection', function(conn) {
     console.log("Connected");
     conn.on( 'data', function(jsonStr) {
         var jsonObj = JSON.parse(jsonStr);
-        var dt = jsonObj.dt;
-        var response = {
-            dt: dt,
-            pos: {
-                dx: 0.0,
-                dy: 0.0
-            },
-            rot: {
-                dx: dt*0.05,
-                dy: dt*0.1
+        var type = jsonObj.type;
+        // Perform a physics update on the dynamic bodies
+        if (type == 'update') {
+            var t = jsonObj.time;
+            var dt = jsonObj.delta;
+            var response = { type: 'update',
+                             delta: dt,
+                             objs: [] };
+            for (var i=0; i<dynamicObjects.length; ++i) {
+                response.objs[i] = {
+                    id: dynamicObjects[i].id,
+                    dx: 0.0,
+                    dy: 0.0,
+                    rx: 0.05,
+                    ry: 0.1
+                };
             }
-        };
-        conn.write(JSON.stringify(response));
+            conn.write(JSON.stringify(response));
+        }
+        // Register dynamic objects
+        else if (type == 'dynamic') {
+            console.log("Registering dynamic object.");
+            console.log(jsonObj);
+            dynamicObjects[jsonObj.id] = new DynamicObject(jsonObj);
+        }
+        // Register static objects
+        else if (type == 'static') {
+            console.log("Registering static object.");
+            console.log(jsonObj);
+            staticObjects[jsonObj.id] = new StaticObject(jsonObj);
+        }
     } );
     conn.on( 'disconnection', function(conn) {
         console.log("Disconnected from:");
@@ -32,10 +74,11 @@ sockjs_server.on( 'connection', function(conn) {
     } );
 } );
 
+
 // 2. Static files server
 var static_directory = new node_static.Server(__dirname);
 
-// 3. Usual http stuff
+// 3. Node HTTP file server
 var node_server = http.createServer();
 node_server.addListener('request', function(req, res) {
     static_directory.serve(req, res);
